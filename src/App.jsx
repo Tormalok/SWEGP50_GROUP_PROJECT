49a5d96c0da7e843e0db078223dfb0d1ffad4bfb
Henry Amponsah <hamponsah010@st.ug.edu.gh>
2024-07-13 06:11:50 +0000

    Set routing for additional pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Header from './components/Header';
+import Login from './Pages/Login';
+import About from './Pages/About';
 
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles/style.css';
+
+const Header = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/login'>Login</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
