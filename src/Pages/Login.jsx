519cad25a82ab45aded01ca60a9ecc089d4194ac
Henry Amponsah <hamponsah010@st.ug.edu.gh>
2024-07-17 23:38:04 +0000

    Added form fields to the form-main section

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -10,7 +10,43 @@ const Login = () => {
         <Header />
 
         <div className='forms-section'>
-          <h1>Fill up here!</h1>
+          <h1 className='title'>Sign in</h1>
+          <form className='form-main'>
+            <div>
+              {/* <label htmlFor='email'>Email:</label> */}
+              <input
+                placeholder='Email'
+                type='email'
+                id='email'
+                // value={email}
+                // onChange={handleEmailChange}
+                required
+              />
+            </div>
+            <div>
+              {/* <label htmlFor='password'>Password:</label> */}
+              <input
+                placeholder='Password'
+                type='password'
+                id='password'
+                // value={password}
+                // onChange={handlePasswordChange}
+                required
+              />
+            </div>
+            {/* <button type='submit'>Sign In</button> */}
+            <div className='btn'>Sign in</div>
+
+            <div className='option'>
+              <p>
+                Don't have an account?
+                <span> Sign up</span>
+              </p>
+            </div>
+            <div className='btn-links'>Sign in with Google</div>
+            <div className='btn-links'>Sign in with Microsoft</div>
+            <div className='btn-links'>Sign in with Apple</div>
+          </form>
         </div>
       </div>
       <Footer />
