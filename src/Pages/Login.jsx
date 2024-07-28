9f895acfb5ad53f322f9bf1125b33c14af949dd9
Henry Amponsah <hamponsah010@st.ug.edu.gh>
2024-07-28 04:14:38 +0000

    Made changes to the login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,58 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import './styles/style.css';
 
 const Login = () => {
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const toggleSignUp = () => {
+    setIsSignUp(!isSignUp);
+  };
+
   return (
     <>
       <div className='login-container'>
         <Header />
 
         <div className='form-section'>
-          <h1 className='title'>Sign in</h1>
+          <h1 className='title'>{isSignUp ? 'Sign up' : 'Sign in'}</h1>
           <form className='form-main'>
             <div>
-              {/* <label htmlFor='email'>Email:</label> */}
-              <input
-                placeholder='Email*'
-                type='email'
-                id='email'
-                // value={email}
-                // onChange={handleEmailChange}
-                required
-              />
+              <input placeholder='Email*' type='email' id='email' required />
             </div>
             <div>
-              {/* <label htmlFor='password'>Password:</label> */}
               <input
                 placeholder='Password*'
                 type='password'
                 id='password'
-                // value={password}
-                // onChange={handlePasswordChange}
                 required
               />
             </div>
-            <button type='submit'>Sign In</button>
-            {/* <div className='btn'>Sign in</div> */}
+            <button type='submit'>{isSignUp ? 'Sign Up' : 'Sign In'}</button>
 
             <div className='option'>
               <p>
-                Don't have an account?
-                <span> Sign up</span>
+                {isSignUp
+                  ? 'Already have an account?'
+                  : "Don't have an account?"}
+                <span onClick={toggleSignUp} className='toggle-link'>
+                  {isSignUp ? ' Sign in' : ' Sign up'}
+                </span>
               </p>
             </div>
             <a href='#' className='btn-links'>
-              <img src='\form_logos\google.svg' />
+              <img src='\form_logos\google.svg' alt='Google logo' />
               <p>Continue with Google</p>
             </a>
             <a href='#' className='btn-links'>
-              <img src='\form_logos\microsoft.svg' />
+              <img src='\form_logos\microsoft.svg' alt='Microsoft logo' />
               <p>Continue with Microsoft Account</p>
             </a>
             <a href='#' className='btn-links'>
-              <img src='\form_logos\apple.svg' />
+              <img src='\form_logos\apple.svg' alt='Apple logo' />
               <p>Continue with Apple</p>
             </a>
           </form>
