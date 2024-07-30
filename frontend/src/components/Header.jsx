4d100cc3b88ff4ec4cceffd4b51c4e25339854c4
Henry <hamponsah010@st.ug.edu.gh>
2024-07-30 01:46:22 +0000

    Added a nav-bar to the header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,20 +7,44 @@ import './styles/style.css';
 
 const Header = () => {
   return (
-    <div className='header'>
-      <div className='left-group'>
-        <div className='logo'>
-          <Link to='/'>
-            <span className='logo-ink'>ARC</span>VAN
-          </Link>
+    <>
+      <div className='header'>
+        <div className='left-group'>
+          <div className='logo'>
+            <Link to='/'>
+              <span className='logo-ink'>ARC</span>VAN
+            </Link>
+          </div>
+          <SearchBox />
         </div>
-        <SearchBox />
+        <div className='right-group'>
+          <Account />
+          <Cart />
+        </div>
       </div>
-      <div className='right-group'>
-        <Account />
-        <Cart />
-      </div>
-    </div>
+      <nav className='nav-bar'>
+        <ul className='nav-links'>
+          <li>
+            <Link to='/categories'>Categories</Link>
+          </li>
+          <li>
+            <Link to='/brands'>Brands</Link>
+          </li>
+          <li>
+            <Link to='/services'>Services</Link>
+          </li>
+          <li>
+            <Link to='/deals'>Deals</Link>
+          </li>
+          <li>
+            <Link to='/about'>About Us</Link>
+          </li>
+          <li>
+            <Link to='/contact'>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
